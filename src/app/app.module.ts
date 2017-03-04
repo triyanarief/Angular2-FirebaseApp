1fc977a762a18a359414f8c24eb731352ec0c879
triyanarief <triyanarief@icloud.com>
2017-03-04 18:29:02 +0700

    api key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {RouterModule, Routes} from '@angular/router';
+import { AngularFireModule} from 'angularfire2';
+import {FirebaseService} from './services/firebase.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -12,6 +14,13 @@ import { ListingComponent } from './components/listing/listing.component';
 import { AddListingComponent } from './components/add-listing/add-listing.component';
 import { EditListingComponent } from './components/edit-listing/edit-listing.component';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyA3XsZ_LK1GMABEinul_PGV95Zf1n1k-74",
+  authDomain: "iklanproperti-e4ea0.firebaseapp.com",
+  databaseURL: "https://iklanproperti-e4ea0.firebaseio.com",
+  storageBucket: "iklanproperti-e4ea0.appspot.com",
+  messagingSenderId: "252503479977"
+};
 
 const appRoutes: Routes = [
   {path:'', component:HomeComponent},
