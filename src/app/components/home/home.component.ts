8641ded1ec0dc6cb1fbfaa3f1079392a32c82d67
triyanarief <triyanarief@icloud.com>
2017-03-06 23:48:35 +0700

    home component update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFire} from 'angularfire2';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-home',
