ba8828bb5c7ebc8cd18c118f6827139eceb2e049
triyanarief <triyanarief@icloud.com>
2017-03-07 14:33:19 +0700

    add-iklan component

--- src/app/components/add-listing/add-listing.component.ts
+++ src/app/components/add-listing/add-listing.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FirebaseService} from '../../services/firebase.service';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-add-listing',
